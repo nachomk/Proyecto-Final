53b861f7c9f29e997fcc23d664323a05baaf7f40
Ignacio Martinez Katzky <nacho.mk@hotmail.com>
2023-09-06 20:39:22 -0300

    cambios

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 const navController = require("./Server/Controllers/navigationController");
 const giftcardController = require("./Server/Controllers/giftcardController");
 const app = express();
-const PORT = process.env.PORT ||8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname , "./Client")))
